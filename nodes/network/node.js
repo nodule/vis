8c216543a865253cd350dc8cade2454e4c8f6f55
Rob Halff <rob.halff@gmail.com>
2014-12-15 01:06:32 +0100

    initial commit

---
+++ nodes/network/node.js
@@ -0,0 +1,27 @@
+output = function() {
+  var network = new vis.Network(
+    input.container,
+    input.data,
+    input.options
+  );
+
+  function event(name) {
+    return function(event) {
+      var out = {};
+      out[name] = event;
+      output(out);
+    };
+  }
+
+  [
+   'select','click','doubleClick','hoverNode',
+   'blurNode','resize','dragEnd','startStabilization',
+   'stabilized','viewChanged','zoom'
+  ].forEach(function(eventName) {
+    // TODO: remove these on shutdown or removal.
+    network.on(eventName, event(eventName));
+  });
+
+  output({network: network});
+
+};
