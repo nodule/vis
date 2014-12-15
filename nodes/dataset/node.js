398c38e062eccd98b3c22c795d6b99f21014416b
Rob Halff <rob.halff@gmail.com>
2014-12-15 05:08:07 +0100

    should work

--- nodes/dataset/node.js
+++ nodes/dataset/node.js
@@ -1,16 +1,5 @@
 state.dataset = new vis.DataSet(/*TODO: options*/);
 
-state.dataset.on('*', function(event, properties, senderId) {
-  // output() func available?
-  output({
-    event: {
-      event: event,
-      properties: properties,
-      senderId: senderId
-    }
-  });
-});
-
 on.input.add = function () {
   state.dataset.add(data);
 };
@@ -21,6 +10,15 @@ on.input.remove = function () {
   state.dataset.update(data);
 };
 
-output = {
-  dataset: state.dataset
-};
+output = fuction() {
+  state.dataset.on('*', function(event, properties, senderId) {
+    // output() func available?
+    cb({
+      event: {
+        event: event,
+        properties: properties,
+        senderId: senderId
+      }
+    });
+  });
+}
